ad965cf8be5981fcb08b4679e0ae5a2683c1db5b
Renato Oliveira <renato.moreira@aluno.ifsp.edu.br>
2024-08-07 10:05:50 -0300

    Adicionar arquivos de projeto.

---
+++ att forms/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace att_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(817, 591);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ att forms/frmlogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace att_forms
+{
+    public partial class frmlogin : Form
+    {
+        public frmlogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmlogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String user = textBox1.Text;
+            String senha = textBox2.Text;  
+
+            if(user == "admin")
+            {
+               if(senha == "admin")
+                {
+                    Form1 form1 = new Form1();
+                    form1.Show();
+                    frmlogin frmlogin = new frmlogin();
+                    frmlogin.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("A senha nao coincide com o login");
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("O login nao coincide com nenhum login registrado");
+            }
+
+            
+        }
+    }
+}
